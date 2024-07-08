5f98c35e34fd64f7d89d793ee29edc974b0b9cc9
2002nitesh <112775047+2002nitesh@users.noreply.github.com>
2024-07-08 11:56:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(8800 || 4400, () => {
+app.listen(process.env.PORT || 8800, () => {
   connect();
   console.log("Connected to Server");
 });
